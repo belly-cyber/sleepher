88a89e4480878d75d58b41f2ec54dbf57bc8c535
lanarhoades-source <67766715+lanarhoades-source@users.noreply.github.com>
2021-09-08 22:15:44 -0400

    Update sleepher.py

--- sleepher.py
+++ sleepher.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-
 import time
 import sys
 
-
 sec_in_24    = 86400
-current_time =time.strftime('%H:%M:%S',time.localtime())
+current_time =time.strftime('%H:%M',time.localtime())
+
 help_menu = """
-sleepher.py [wake-up time] [-e[number of days]]
-    example: 'python3 sleepher.py'
-             'python3 sleepher.py -e'
-             'python3 sleepher.py -e 4'
-             python3 sleepher.py 13:00'
-    
-        'wake-up' default time is 8:00am
-        -e(weekEnd) default time is two days
-        -h shows this message
+sleepher.py [wake-up time] [-e[number of days off]]
 
+    example: 'python3 sleepher.py 08:00'
+    'wake-up' default time is 08:00
+    -e(weekEnd) default time is two days
+    -h shows this message
 """
+for argv in sys.argv:
+    if ':' in argv:
+        wakeup_time=argv
+        sys.argv.remove(argv)
+        break 
+    else:
+        wakeup_time  = '08:00'
 
-if len(sys.argv) >1 and len(sys.argv[1])>=4:
-    wakeup_time = sys.argv[1]
-else:
-    wakeup_time  = '08:00'
+def to_seconds(t):
+    '''input = time in 00:00 format
+       outputs into seconds
+    '''
+    return sum([(60**i) * int(seg) for i,seg in enumerate(t.split(':')[::-1],start=1)])
 
-def overnight_sleep(c_time=current_time,wake_time=wakeup_time):
-    CH,CM,CS = map(int,c_time.split(':'))
-    WH,WM = map(int,wake_time.split(':'))
-    seconds = sec_in_24 - (CH*60*60+CM*60+CS) + (WH*60*60 + WM*60)
-    return seconds
+def overnight_sleep():
+    return sec_in_24 + to_seconds(wakeup_time) - to_seconds(current_time)
 
 def weekend_sleep(days_off):
-    seconds = overnight_sleep() + days_off*sec_in_24
-    return seconds
-
+    return overnight_sleep() + (sec_in_24 * days_off)
     
 try:
     if '-h' in sys.argv:
         print(help_menu)
     elif '-e' in sys.argv:
-        if sys.argv.index('-e')+1 <len(sys.argv) :
-            num_days=int(sys.argv[sys.argv.index('-e')+1])
+        if sys.argv.index('-e') + 1 < len(sys.argv):
+            num_days = int(sys.argv[sys.argv.index('-e')+1])
         else:
-            num_days=2
-        print(weekend_sleep(days_off=num_days))
+            num_days = 2
+        print(weekend_sleep(num_days))
     else:
         print(overnight_sleep())
 except:print(help_menu)
